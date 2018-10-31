7e289d7615408fcff6563f63793ae3d0947138e9
Christian Henry <chenry5724@gmail.com>
2018-10-31 16:05:23 -0500

    baby steps

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+let friends = [
+   {
+        "name": "Vincent",
+        "photo":"https://www.cheatsheet.com/wp-content/uploads/2014/06/Pulp_Fiction_Marvin_640x360_64275523687.jpg?x16659",
+        "scores":[5,2,4,2,4,1,4,3,5,4,]
+    },
+    {
+        "name": "Jules",
+        "photo":"https://i.redd.it/f5htnwolsf211.png",
+        "scores":[5,2,4,2,4,1,4,3,5,4,]
+    },
+    {
+        "name": "Butch",
+        "photo":"https://medias.spotern.com/spots/w640/12275-1532336916.jpg",
+        "scores":[5,2,4,2,4,1,4,3,5,4,]
+    },
+    {
+        "name": "Mia",
+        "photo":"http://fr.web.img6.acsta.net/medias/nmedia/18/36/02/52/18889985.jpg",
+        "scores":[5,2,4,2,4,1,4,3,5,4,]
+    },
+    {
+        "name": "Marcelus",
+        "photo":"https://pbs.twimg.com/profile_images/911009806038847488/gOYhvzom_400x400.jpg",
+        "scores":[5,2,4,2,4,1,4,3,5,4,]
+    }
+]
+module.exports = friends
